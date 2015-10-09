2977e80060a72cbc5379288b8340c80cb4b7a429
curlybrake <asim.dogan.namli@gmail.com>
2015-10-09 05:16:50 +0300

    Gzip
    sıkıştırma test

--- themes/first/index.php
+++ themes/first/index.php
@@ -1,11 +1,132 @@
-<!doctype html>
-<html>
-    <head>
-        <script src="../../system/client/AjaxCan.js"></script>
-        <script src="../../system/client/history.min.js"></script>
+<?php
+    ob_start("ob_gzhandler");
+?>
+<head>
+    <style>
+        #div821212810904 {
+            border: 1px dotted gray;
+            position: absolute;
+            width: 0px;
+            height: 1px;
+            left: 551.5px;
+            top: 869.5px;
+        }
+    </style>
+    <script src="../../system/client/AjaxCan.js"></script>
+    <script src="../../system/client/history.min.js"></script>
+    <script src="../../system/client/jquery-1.11.2.min.js"></script>
+    <link href="css/first.css" rel="stylesheet" type="text/css">
+    <link href="css/firstresponsive.css" rel="stylesheet" type="text/css">
+</head>
 
-    </head>
-    <body>
-        
-    </body>
-</html>
+<body>
+    <div class="center">
+        <div id="background">
+
+        </div>
+    </div>
+
+    <div class="content">
+        <div id="first" class="dynaCan responsive">
+            <div id="scheduleCan">
+                <table border="0">
+                    <tr class="dark head">
+                        <td class="dark time"></td>
+                        <td>Pazartesi</td>
+                        <td class="dark">Salı</td>
+                        <td>Çarşamba</td>
+                        <td class="dark">Perşembe</td>
+                        <td>Cuma</td>
+                    </tr>
+                    <tr>
+                        <td class="dark time">8:40</td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                    </tr>
+                    <tr class="dark">
+                        <td class="dark time">9:40</td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td class="dark time">10:40</td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                    </tr>
+                    <tr class="dark">
+                        <td class="dark time">11:40</td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td class="dark time">12:40</td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                    </tr>
+                    <tr class="dark">
+                        <td class="dark time">13:40</td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td class="dark time">14:40</td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                    </tr>
+                    <tr class="dark">
+                        <td class="dark time">15:40</td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td class="dark time">16:40</td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                    </tr>
+                    <tr class="dark">
+                        <td class="dark time">17:40</td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                        <td class="dark"></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div id="second" class="dynaCan responsive">
+            
+        </div>
+        <div id="third" class="dynaCan responsive">
+
+        </div>
+    </div>
+    
+</body>
